ec6d63e0be253a171d90c9e1e394b606a5d0b627
krupinskij <jk.krupinski@gmail.com>
2020-04-14 16:40:14 +0200

    Configure Firebase

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,38 @@
 <script>
-	import StartPage from './pages/StartPage.svelte';
-	import LevelPage from './pages/LevelPage.svelte';
-	import GamePage from './pages/GamePage.svelte';
-	import ResultPage from './pages/ResultPage.svelte';
+  import StartPage from "./pages/StartPage.svelte";
+  import LevelPage from "./pages/LevelPage.svelte";
+  import GamePage from "./pages/GamePage.svelte";
+  import ResultPage from "./pages/ResultPage.svelte";
+
+//   import { FirebaseApp, User, Doc, Collection } from "sveltefire";
+//   import firebase from "firebase/app";
+//   import "firebase/firestore";
+//   import "firebase/auth";
+//   import "firebase/performance";
+//   import "firebase/analytics";
+
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyAxrgJ51USj65rHbYIjEoX0XrMLbPHpnSI",
+//     authDomain: "pokemon-memory-game-34ac6.firebaseapp.com",
+//     databaseURL: "https://pokemon-memory-game-34ac6.firebaseio.com",
+//     projectId: "pokemon-memory-game-34ac6",
+//     storageBucket: "pokemon-memory-game-34ac6.appspot.com",
+//     messagingSenderId: "314667407796",
+//     appId: "1:314667407796:web:a8b4f315c817d15158fae1"
+//   };
+
+//   firebase.initializeApp(firebaseConfig);
 </script>
 
-<main>
-	<StartPage/>
-	<!-- <LevelPage/> -->
-	<!-- <GamePage/> -->
-	<!-- <ResultPage/> -->
-</main>
-
 <style>
+
 </style>
+
+<main>
+  <!-- <FirebaseApp {firebase}> -->
+    <StartPage />
+    <!-- <LevelPage/> -->
+    <!-- <GamePage/> -->
+    <!-- <ResultPage/> -->
+  <!-- </FirebaseApp> -->
+</main>
