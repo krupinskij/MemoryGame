dbd291eef0f987d222bc17263c22bcf5e2187cf5
Jan Krupinski <jk.krupinski@gmail.com>
2021-03-02 20:35:53 +0100

    Move firebase config variables to dotenv file

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
