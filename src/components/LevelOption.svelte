3ef68a96fb68f94966f01dbfe13ef5e7e246ca21
krupinskij <jan.krupinski@in-team.com.pl>
2021-06-26 19:24:06 +0200

    🌐 Optimization: Use font with polish letters

--- src/components/LevelOption.svelte
+++ src/components/LevelOption.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class={`${selected ? 'text-6xl underline' : 'text-4xl'} cursor-pointer hover:font-black`}
+  class={`${selected ? 'text-6xl underline' : 'text-4xl'} cursor-pointer hover:font-black m-2`}
   on:click={onClick}
 >
   <slot></slot>
