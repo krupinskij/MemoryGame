ef3d93130c558c7ed091842c3ffeb3c46499cefd
krupinskij <jan.krupinski@in-team.com.pl>
2021-06-26 18:42:02 +0200

    ⚡️ Optimization: Optimize colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray
+    },
     extend: {
       backgroundImage: {
         'game': "url('/img/background/bg-game.png')",
